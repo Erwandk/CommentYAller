18c2caa48ccf07d1c208de5a794b4b1343caed83
gabdid <gabrielle.didelot@student.ecp.fr>
2018-10-12 09:32:45 +0200

    Classe meteo modifiee

--- APIs/class_meteo.py
+++ APIs/class_meteo.py
@@ -1,39 +1,36 @@
 import requests
-import class_api
+from APIs.class_api import API
 import time
 
 
-class Meteo(class_api.API):
+class Meteo(API):
 
     def __init__(self):
-        self.url = 'http://www.infoclimat.fr/public-api/gfs/json?_ll=48.85341,2.3488&_auth=CBIAFwN9BiRRfFtsA3ULIgBoBTBdK1dwA38CYVs%2BB3pWPVAxVDRTNVY4VyoFKlVjWXQFZl5lUmIBagZ%2BCHpXNghiAGwDaAZhUT5bPgMsCyAALgVkXX1XcANhAmZbPgd6VjRQPVQyUy9WO1cxBTdVf1loBWdeYFJ1AX0GYAhgVzcIYwBjA2MGZVE9WzADMgsgACwFYF0wVz0DZwJnW2IHY1ZjUDdUMlMxVjtXZwUwVX9ZYwVkXmBSaAFrBmQIZ1c1CHQAewMZBhdRI1t5A3ELagB1BXhdN1cxAzQ%3D&_c=cc703eb909f9f00058063c7c570f6a45'
-        self.nom = 'meteo'
+        API.__init__(self, url='http://www.infoclimat.fr/public-api/gfs/json?_ll=48.85341,2.3488&_auth=CBIAFwN9BiRRfFtsA3ULIgBoBTBdK1dwA38CYVs%2BB3pWPVAxVDRTNVY4VyoFKlVjWXQFZl5lUmIBagZ%2BCHpXNghiAGwDaAZhUT5bPgMsCyAALgVkXX1XcANhAmZbPgd6VjRQPVQyUy9WO1cxBTdVf1loBWdeYFJ1AX0GYAhgVzcIYwBjA2MGZVE9WzADMgsgACwFYF0wVz0DZwJnW2IHY1ZjUDdUMlMxVjtXZwUwVX9ZYwVkXmBSaAFrBmQIZ1c1CHQAewMZBhdRI1t5A3ELagB1BXhdN1cxAzQ%3D&_c=cc703eb909f9f00058063c7c570f6a45', nom='meteo')
+
+    def _set_time(self):
+        t = time.localtime()
+        # Date
+        date = time.strftime('20%y-%m-%d')
+        # Créneau horaire
+        if t[3] % 3 == 0:  # Cas des heures suivantes : 00h, 3h, 6h, 9h, 12h, 15h, 18h, 21h
+            heure = str(t[3] - 1) + ':00:00'
+        elif t[3] % 3 == 1:  # Cas des heures suivantes : 1h, 4h, 7h, 10h, 13h, 16h, 19h, 22h
+            heure = str(t[3] - 2) + ':00:00'
+        elif t[3] % 3 == 2:
+            heure = time.strftime('%H:00:00')
+        dateheure = date + ' ' + heure
+        print(dateheure)
+        return dateheure
 
     def get_infos(self):
-        #  TODO : à construire à partir de la méthode get_infos de la classe API
         # Connexion à l'API
         resp = requests.get(self.url)
         if resp.status_code != 200:
             # This means something went wrong.
             raise NotImplementedError(
                 "Erreur {} : vous n'avez pas réussi à vous connecter à l'url {}.".format(resp.status_code, self.url))
-        # Paramétrage du temps (date et heure)
-        t = time.localtime()
-        # Date
-        if len(str(t[2]))<2: # pour passer de 2018-10-7 à 2018-10-07
-            a = '0'
-        else:
-            a = ''
-        date = str(t[0]) + '-' + str(t[1]) + '-' + a + str(t[2]) # au format 'aaaa-mm-jj'
-        # Créneau horaire
-        if t[3]%3 == 0: # Cas des heures suivantes : 00h, 3h, 6h, 9h, 12h, 15h, 18h, 21h
-            heure = str(t[3]-1) + ':00:00'
-        elif t[3]%3 == 1: # Cas des heures suivantes : 1h, 4h, 7h, 10h, 13h, 16h, 19h, 22h
-            heure = str(t[3]-2) + ':00:00'
-        elif t[3]%3 == 2:
-            heure = str(t[3]) + ':00:00'
-        dateheure = date + ' ' + heure
-        print(dateheure)
+        dateheure = self._set_time()
         # Extraction des informations
         weather_data = resp.json()[dateheure]
         return weather_data
